5b8f1ba2d80f58eee93c5339f40d721cbdb6b681
thanhhngu <thanhhhngu.06@gmail.com>
2026-09-19 23:58:36 +0700

    connect db-sqlserver with db name DotJobDB

---
+++ BTL_LTW-DOTJOB/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BTL_LTW_DOTJOB.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- BTL_LTW-DOTJOB/Program.cs
+++ BTL_LTW-DOTJOB/Program.cs
@@ -1,6 +1,14 @@
+using BTL_LTW_DOTJOB.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
